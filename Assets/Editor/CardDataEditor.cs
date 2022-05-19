68d0faed783e4839fcb536aeea520de29c810802
Mortiss <veronikabyzova@gmail.com>
2022-05-19 18:33:21 +0500

    Path Label on Card Data assets

--- Assets/Editor/CardDataEditor.cs
+++ Assets/Editor/CardDataEditor.cs
@@ -12,6 +12,7 @@ namespace Editor
             //cooldown in one row
             //event rate arrays all visible
             base.OnInspectorGUI();
+            
             if (GUILayout.Button("Save", GUILayout.Width(200)))
             {
                 CardData card = target as CardData;
@@ -20,6 +21,7 @@ namespace Editor
             bool autoUpdate = EditorPrefs.HasKey("AutoUpdate") && EditorPrefs.GetBool("AutoUpdate");
             string hint = autoUpdate ? "Auto Update enabled" : "No Auto Update";
             GUILayout.Label(hint);
+            GUILayout.Label($"Save to {EditorPrefs.GetString("CSVPath")}");
         }
     }
 }
